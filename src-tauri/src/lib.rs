2f52bdc9ebfb60ab721d877929f522238c61b41c
nabbisen <nabbisen@scqr.net>
2025-01-04 21:16:10 +0900

    devtools in debug build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 // #[tauri::command]
 // fn greet(name: &str) -> String {
@@ -7,6 +9,16 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+
+            #[cfg(debug_assertions)]
+            {
+                window.open_devtools();
+            }
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         // .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
