0ddeff614ab14ab0bdeaeb7a697e87346911ab26
nabbisen <nabbisen@scqr.net>
2025-01-26 11:31:02 +0900

    heading navs; global styles

--- src/components/BlockLeading.svelte
+++ src/components/BlockLeading.svelte
@@ -1,30 +1,61 @@
 <script lang="ts">
   const {
+    nesting_level,
     heading_level,
     heading_text,
-    onchange,
-  }: { heading_level: number; heading_text: string; onchange: Function } = $props()
+    visibleLevel,
+    textOnchange,
+    visibleLevelOnChange,
+  }: {
+    nesting_level: number
+    heading_level: number
+    heading_text: string
+    visibleLevel: number | null
+    textOnchange: Function
+    visibleLevelOnChange: Function
+  } = $props()
 </script>
 
-{#if heading_level <= 6}
-  <svelte:element
-    this={`h${heading_level}`}
-    onblur={(e: FocusEvent & { currentTarget: EventTarget & HTMLElement }) =>
-      onchange(e.currentTarget.innerText)}
-    contenteditable
-  >
-    {heading_text}
-  </svelte:element>
-{:else}
-  <div
-    class={`h${heading_level}`}
-    onblur={(e: FocusEvent & { currentTarget: EventTarget & HTMLElement }) =>
-      onchange(e.currentTarget.innerText)}
-    contenteditable
-  >
-    {heading_text}
+<div class="d-flex">
+  <header>
+    <nav>
+      <button onclick={() => visibleLevelOnChange(nesting_level + 1)}
+        >{visibleLevel && nesting_level === visibleLevel - 1 ? '+' : '-'}</button
+      >
+      <button
+        class={!visibleLevel ? 'rotate-90' : nesting_level < visibleLevel - 1 ? 'rotate-90' : ''}
+        >></button
+      >
+    </nav>
+  </header>
+  <div class="content">
+    {#if heading_level <= 6}
+      <svelte:element
+        this={`h${heading_level}`}
+        onblur={(e: FocusEvent & { currentTarget: EventTarget & HTMLElement }) =>
+          textOnchange(e.currentTarget.innerText)}
+        contenteditable
+      >
+        {heading_text}
+      </svelte:element>
+    {:else}
+      <div
+        class={`h${heading_level}`}
+        onblur={(e: FocusEvent & { currentTarget: EventTarget & HTMLElement }) =>
+          textOnchange(e.currentTarget.innerText)}
+        contenteditable
+      >
+        {heading_text}
+      </div>
+    {/if}
   </div>
-{/if}
+  <footer>
+    <nav>
+      <button>+-</button>
+      <button>+|</button>
+    </nav>
+  </footer>
+</div>
 
 <style>
   h1,
@@ -35,5 +66,23 @@
   h6 {
     padding: 0;
     margin: 0;
+  }
+
+  nav {
+    padding: 0 1.1rem;
+  }
+  nav button {
+    background: none;
+    border: none;
+    font-size: 1.1rem;
+  }
+  footer nav {
+    display: none;
+    margin-left: -1.2rem;
+  }
+  nav,
+  .content:hover ~ footer nav,
+  footer nav:hover {
+    display: flex;
   }
 </style>
--- src/components/Editor/index.svelte
+++ src/components/Editor/index.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import { onMount } from 'svelte'
   import { invoke } from '@tauri-apps/api/core'
-  import BlockLeading from './BlockLeading.svelte'
-  import BlockContent from './BlockContent.svelte'
-  import { type ParsedMarkdown } from './Editor'
+  import BlockLeading from '../BlockLeading.svelte'
+  import BlockContent from '../BlockContent.svelte'
+  import { type ParsedMarkdown } from './types'
+  import { maxNestingLevel as getMaxNestingLevel, visible } from './scripts'
 
   onMount(() => {
     invoke('ready', {})
@@ -35,24 +36,11 @@
       })
   }
 
-  let visibleLevel: number | null = $state(null)
-
-  const visible = (block: ParsedMarkdown): boolean => {
-    if (!visibleLevel || Number.isNaN(visibleLevel) || Number(visibleLevel) <= 0) {
-      return true
-    }
-    return block.heading_level ? block.heading_level <= Number(visibleLevel) : false
-  }
-
   let content: string = $state('')
   let parsedMarkdowns: ParsedMarkdown[] = $state([])
-  let maxNestingLevel = $derived.by(() => {
-    let maxLevel: number = 0
-    parsedMarkdowns.forEach((x: ParsedMarkdown) => {
-      if (maxLevel < x.nesting_level) maxLevel = x.nesting_level
-    })
-    return maxLevel
-  })
+  let maxNestingLevel = $derived.by(() => getMaxNestingLevel(parsedMarkdowns))
+
+  let visibleLevel: number | null = $state(null)
 
   const onchange = (value: string, index: number, isHeading: boolean) => {
     if (isHeading && parsedMarkdowns[index].heading_text === value) return
@@ -68,12 +56,23 @@
     <div class="col">
       {#each parsedMarkdowns as block, i}
         <div class={`nested nest-${block.nesting_level}`}>
-          {#if visible(block)}
+          {#if visible(block, visibleLevel)}
             {#if block.heading_level && 0 < block.heading_level}
               <BlockLeading
+                nesting_level={block.nesting_level}
                 heading_level={block.heading_level}
                 heading_text={block.heading_text ?? ''}
-                onchange={(value: string) => onchange(value, i, true)}
+                {visibleLevel}
+                textOnchange={(value: string) => {
+                  onchange(value, i, true)
+                }}
+                visibleLevelOnChange={(value: number) => {
+                  if (visibleLevel === value) {
+                    visibleLevel = null
+                  } else {
+                    visibleLevel = value
+                  }
+                }}
               />
             {:else}
               <BlockContent
@@ -89,5 +88,5 @@
 </main>
 
 <style>
-  @import './Editor.css';
+  @import './styles.css';
 </style>
